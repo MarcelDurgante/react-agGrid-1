7fac188fefb397e7075b64f2e04119dae79713c1
MarcelDurgante <marceldur@gmail.com>
2023-07-13 21:43:18 +0200

    Table component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import './App.css'
+import './App.css';
+import { Table } from './components/Table';
 
 function App() {
-
   return (
     <>
-      Ag Grid
+      <h1>React App</h1>
+      <div className='App'>
+        <Table />
+      </div>
     </>
   )
 }
 
-export default App
+export default App;
